411ddd4ebd3fca58d72bd372690972ea4a589208
xelopsys <juraqulovjamshid7@gmail.com>
2022-06-09 22:23:02 +0500

    auth is built and updated API request, bugs fixed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,16 +1,17 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { config } from "../../../config"
 
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
         GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+            clientId: config.GITHUB_ID || process.env.GITHUB_ID,
+            clientSecret: config.GITHUB_SECRET || process.env.GITHUB_SECRET,
         }),
         // ...add more providers here
     ],
     jwt: {
-        signinKey: process.env.JWT_SIGNING_PRIVATE_KEY,
+        signinKey: config.JWT_SIGNING_PRIVATE_KEY || process.env.JWT_SIGNING_PRIVATE_KEY,
     }
 })
